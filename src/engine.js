6f155814f6d5c17dfc4bd2e1ccae8d5e838130d6
djumaoas <davidjumiller@gmail.com>
2021-11-08 16:21:42 -0800

    Added file reads and writes for engine

--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,17 @@
 let acorn = require("acorn");
+let fs = require("fs");
 
-let input = "1 + 1"; // Test input for now
+// Async read js file from input directory
+fs.readFile(__dirname + "/inputs/test1.js", "utf8", (err, input) => {
+    let data;
+    if (err) { console.log(err); } else {
+        data = JSON.stringify(acorn.parse(input, {ecmaVersion: 2020, sourceType: "module"}), null, 4);
+    }
 
-console.log(acorn.parse(input, {ecmaVersion: 2020}));
+    // Async write json file to outputs directory
+    fs.writeFile(__dirname + "/outputs/test1out.json",  data, (err) => {
+        if (err) { console.log(err); } else {
+            console.log("done write");
+        }
+    });
+});
---
+++ src/inputs/test1.js
@@ -0,0 +1,17 @@
+import test1 from "test1";
+import * as test3 from "test3";
+let test4 = require("test4");
+
+let tips = [
+  "Click on any AST node with a '+' to expand it",
+
+  "Hovering over a node highlights the \
+   corresponding location in the source code",
+
+  "Shift click on an AST node to expand the whole subtree"
+];
+
+function printTips() {
+  tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+  tips();
+}
