653e67a673ce9c754863a7b1f52cdc7497ab3fc6
djumaoas <davidjumiller@gmail.com>
2021-11-24 14:22:13 -0800

    work done for adding basic functions and function calls

--- src/Visitor.js
+++ src/Visitor.js
@@ -1,21 +1,108 @@
 export default class Visitor {
+    constructor(output, curFileID) {
+        this.curFileID = curFileID;
+        this.output = output;
+    }
     visitNodes(nodes) { for (let node of nodes) this.visitNode(node); }
     visitNode(node) {
         switch (node.type) {
             case 'Program': return this.visitProgram(node);
+            case 'ImportDeclaration': return this.visitImportDeclaration(node);
+            case 'VariableDeclaration': return this.visitVariableDeclaration(node);
+            case 'VariableDeclarator': return this.visitVariableDeclarator(node);
+            case 'ExportNamedDeclaration': return this.visitExportNamedDeclaration(node);
+            case 'FunctionDeclaration': return this.visitFunctionDeclaration(node);
+            case 'Identifier': return this.visitIdentifier(node);
+            case 'Literal': return this.visitLiteral(node);
+            case 'ExpressionStatement': return this.visitExpressionStatement(node);
+            case 'CallExpression': return this.visitCallExpression(node);
             // TODO: Rest of the switch cases for node visitor
         }
     }
-    visitProgram(node) { return this.visitNodes(node.body); }
+    visitProgram(node) { 
+        this.visitNodes(node.body); 
+        return;
+    }
 
     // TODO: Check here for require statements
     visitVariableDeclaration(node){ return this.visitNodes(node.declarations) }
-    visitVariableDeclarator(){}
+    visitVariableDeclarator(node){}
 
     // TODO: Check here for imports
     visitImportDeclaration(node){}
 
-    visitIdentifier(node){ return node.name; };
+    // TODO: Check here for functions
+    visitFunctionDeclaration(node){
+        let name = node.id.name;
+        let paramCount = node.params.length;
+        let signature = name + "(" + paramCount + ")";
+
+        let newFunction = {
+            "signature": signature,
+            "calledBy": []
+        }
+
+        this.output.forEach(file => {
+            if (file.id == this.curFileID){
+                file.functions.push(newFunction);
+            }
+        });
+
+        return this.visitNodes(node.body.body);
+    }
+
+    visitExpressionStatement(node){ return this.visitNode(node.expression) }
+    visitCallExpression(node){
+        let calleeSignature, paramCount, name;
+        if (node.callee.type == "Identifier") {
+            paramCount = node.arguments.length;
+            name = node.callee.name;
+            calleeSignature = name + "(" + paramCount + ")";
+        } else if (node.callee.type == "MemberExpression") {
+            // TODO
+            return;
+        } else {
+            console.log("Unrecognized type in CallExpression");
+            return;
+        }
+
+        let validFunctionFound = false;
+        // Search each "file" object for the appropriate function that is being called
+        this.output.forEach(file => {
+            // Don't check our temp object
+            if (file.id != "temp"){
+                file.functions.forEach(func => {
+                    if (func.signature == calleeSignature) {
+                        validFunctionFound = true;
+                        // Check if this function has been called in this file already, if so, increment countRef,
+                        // and add the line number
+                        let calledBefore = false;
+                        func.calledBy.forEach(call => {
+                            if (call.id == this.curFileID){
+                                call.countRefs++;
+                                call.atLineNum.push(node.loc.start.line);
+                                calledBefore = true;
+                            }
+                        });
+                        // If no calls to the function from our current file are found, create a new object and push
+                        if (calledBefore == false){
+
+                            let calledByObj = {
+                                "id": this.curFileID,
+                                "atLineNum": [node.loc.start.line],
+                                "countRefs": 1
+                            };
+                            func.calledBy.push(calledByObj);
+                        } else {
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+    visitExportNamedDeclaration(node){}
+    visitIdentifier(node){ return node.name; }
 
     visitLiteral(node){/* Do nothing for now */};
 }
--- src/engine.js
+++ src/engine.js
@@ -1,20 +1,49 @@
-let acorn = require("acorn");
-let fs = require("fs");
-let Visitor = require("./Visitor.js");
+import * as acorn from 'acorn';
+import * as fs from 'fs';
+import Visitor from './Visitor.js';
+
+// Our output to send to visualizer
+let objArrayOutput = [];
+let fileID = 0;
+
+let tempObj = {
+    "id": "temp",
+    "calls": []
+}
+
+objArrayOutput.push(tempObj);
 
 // Async read js file from input directory
-fs.readFile(__dirname + "/inputs/test1.js", "utf8", (err, input) => {
-    let data;
+fs.readFile("./src/inputs/test1.js", "utf8", (err, input) => {
     if (err) { console.log(err); } else {
-        let parsedInput = acorn.parse(input, {ecmaVersion: 2020, sourceType: "module"});
-        data = JSON.stringify(parsedInput, null, 4);
-        let visitor = new Visitor();
+
+        // Parse javascript into acorn's JSON format (AST) 
+        let parsedInput = acorn.parse(input, {ecmaVersion: 2020, sourceType: "module", locations: true});
+
+        // Create a new "file" object for our output
+        fileID++;
+        let fileObjSkeleton = {
+            "id": fileID,
+            "filePath": "TODO",
+            "functions": [],
+            "imports": [],
+            "importedInFiles": []
+        }
+
+        // Push object to output array
+        objArrayOutput.push(fileObjSkeleton);
+
+        /** Traverse AST, fill in the above skeleton with functions found.
+        *   If imports are found, or calls are made, find the corresponding
+        *   "file" object and add the values there.
+        */
+        let visitor = new Visitor(objArrayOutput, fileID);
         visitor.visitNode(parsedInput);
     }
 
-    // Async write json file to outputs directory
+    // Async write json output file to outputs directory
     // **Just for testing purposes, delete later
-    fs.writeFile(__dirname + "/outputs/test1out.json",  data, (err) => {
+    fs.writeFile("./src/outputs/test1out.json",  JSON.stringify(objArrayOutput, null, 4), (err) => {
         if (err) { console.log(err); } else {
             console.log("done write");
         }
