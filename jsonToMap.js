c9aad02bd473d5139640ffff58aa192a24a7f8a6
HowardBian <howardbian623@gmail.com>
2021-11-29 03:06:19 -0800

    added function getCallHierarchy that will take a fileId, signature, and the json and return an object that show's what functions calls the function with the signature in the specified file

--- jsonToMap.js
+++ jsonToMap.js
@@ -65,6 +65,133 @@ const getWhatKeyFileCalls = (json) => {
   return whatKeyFileCalls;
 }
 
+const getCallHierarchy = (fileId, signature, json) => {
+  let file;
+  for (const jsonElement of json) {
+    if (jsonElement.id === fileId) {
+      file = jsonElement;
+      break;
+    }
+  }
+
+  if (file === undefined) {
+    throw new Error("Cannot find file Id");
+  }
+
+  let theFunc;
+  const funcArr = file.functions;
+
+  for (const func of funcArr) {
+    if (func.signature === signature) {
+      theFunc = func;
+      break;
+    }
+  }
+
+  if (theFunc === undefined) {
+    throw new Error("Cannot find the function");
+  }
+
+  let output = {};
+  output.displayName = signature;
+  output.type = theFunc.type;
+  output.fileId = fileId;
+  output.calledBy = [];
+
+  for (const calledByElement of theFunc.calledBy) {
+    let calledByFunc = findCallingFunc(calledByElement.id, calledByElement.atLineNum, json, [signature]);
+    if (typeof(calledByFunc) !== "string") {
+      if (output.calledBy.length === 0) {
+        output.calledBy = calledByFunc;
+      } else {
+        output.calledBy = output.calledBy.concat(calledByFunc);
+      }
+    } else {
+      let loopBack = {};
+      loopBack.displayName = calledByFunc + "[loop back]";
+      loopBack.type = null;
+      loopBack.fileId = null;
+      loopBack.calledBy = null;
+      output.calledBy.push(loopBack);
+    }
+  }
+
+  return output;
+}
+
+const findCallingFunc = (fileId, lineNumArr, json, visited) => {
+  let file;
+  for (const jsonElement of json) {
+    if (jsonElement.id === fileId) {
+      file = jsonElement;
+      break;
+    }
+  }
+
+  if (file === undefined) {
+    throw new Error("Cannot find file Id");
+  }
+
+  let functionsCalling = [];
+
+  let linesWithFunction = [];
+
+  const funcArr = file.functions;
+  for (const func of funcArr) {
+    const start = func.startLine;
+    const end = func.endLine;
+
+    for (const lineNumArrElement of lineNumArr) {
+      if (start <= lineNumArrElement && lineNumArrElement <= end) {
+
+        linesWithFunction.push(lineNumArrElement)
+
+        if (visited.includes(func.signature)) {
+          return func.signature;
+        }
+
+        let theFunc = {};
+        theFunc.displayName = func.signature;
+        theFunc.type = func.type;
+        theFunc.fileId = fileId;
+        theFunc.calledBy = [];
+        for (const calledByElement of func.calledBy) {
+          visited.push(func.signature);
+          let calledByFunc = findCallingFunc(calledByElement.id, calledByElement.atLineNum, json, visited);
+          if (typeof(calledByFunc) !== "string") {
+            if (theFunc.calledBy.length === 0) {
+              theFunc.calledBy = calledByFunc;
+            } else {
+              theFunc.calledBy = theFunc.calledBy.concat(calledByFunc);
+            }
+          } else {
+            let loopBack = {};
+            loopBack.displayName = calledByFunc + "[loop back]";
+            loopBack.type = null;
+            loopBack.fileId = null;
+            loopBack.calledBy = null;
+            theFunc.calledBy.push(loopBack);
+          }
+        }
+        functionsCalling.push(theFunc);
+
+      }
+    }
+  }
+
+  for (const lineNum of lineNumArr) {
+    if (!linesWithFunction.includes(lineNum)) {
+      let theLine = {};
+      theLine.displayName = "Line " + lineNum;
+      theLine.type = null;
+      theLine.fileId = fileId;
+      theLine.calledBy = [];
+      functionsCalling.push(theLine);
+    }
+  }
+  return functionsCalling;
+}
+
 
 const json = [
   {
@@ -73,6 +200,9 @@ const json = [
     "functions": [
       {
         "signature": "foo(x)",
+        "type": "O",
+        "startLine": 135,
+        "endLine": 150,
         "calledBy": [
           {
             "id": 2,
@@ -83,7 +213,7 @@ const json = [
             "countRefs": "3"
           },
           {
-            "id": 5,
+            "id": 3,
             "atLineNum": [
               10
             ],
@@ -93,6 +223,9 @@ const json = [
       },
       {
         "signature": "foo(x, y)",
+        "type": "P",
+        "startLine": 99,
+        "endLine": 111,
         "calledBy": [
           {
             "id": 2,
@@ -106,9 +239,7 @@ const json = [
     ],
     "importedInFiles": [
       2,
-      3,
-      5,
-      10
+      3
     ]
   },
   {
@@ -117,6 +248,9 @@ const json = [
     "functions": [
       {
         "signature": "blah(x)",
+        "type": "O",
+        "startLine": 95,
+        "endLine": 110,
         "calledBy": [
           {
             "id": 1,
@@ -127,7 +261,7 @@ const json = [
             "countRefs": "2"
           },
           {
-            "id": 10,
+            "id": 2,
             "atLineNum": [
               1
             ],
@@ -137,9 +271,12 @@ const json = [
       },
       {
         "signature": "bobTheBuilder(x, y)",
+        "type": "Bam",
+        "startLine": 135,
+        "endLine": 155,
         "calledBy": [
           {
-            "id": 5,
+            "id": 3,
             "atLineNum": [
               100
             ],
@@ -156,9 +293,8 @@ const json = [
       }
     ],
     "importedInFiles": [
-      3,
-      5,
-      10
+      1,
+      3
     ]
   },
   {
@@ -167,9 +303,12 @@ const json = [
     "functions": [
       {
         "signature": "hornet(x)",
+        "type": "P",
+        "startLine": 135,
+        "endLine": 150,
         "calledBy": [
           {
-            "id": 5,
+            "id": 1,
             "atLineNum": [
               110,
               125
@@ -186,7 +325,10 @@ const json = [
         ]
       },
       {
-        "signature": "CornTheCorner(x, y)",
+        "signature": "cornTheCorner(x, y)",
+        "type": "AB",
+        "startLine": 135,
+        "endLine": 150,
         "calledBy": [
           {
             "id": 2,
@@ -196,7 +338,7 @@ const json = [
             "countRefs": "1"
           },
           {
-            "id": 10,
+            "id": 3,
             "atLineNum": [
               69
             ],
@@ -206,12 +348,15 @@ const json = [
       }
     ],
     "importedInFiles": [
-      3,
-      5,
-      10
+      1,
+      2
     ]
   }
 ]
 
-getFilesThatCallKeyFile(json);
-getWhatKeyFileCalls(json);
+//getFilesThatCallKeyFile(json);
+//getWhatKeyFileCalls(json);
+
+let result = getCallHierarchy(1, "foo(x)", json);
+console.log(JSON.stringify(result, null, 4));
+
